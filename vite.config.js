46b5648af29d2138126b9a9e0a9ee18e67e18910
cthr <79426670+cthr@users.noreply.github.com>
2021-03-17 02:03:21 +0000

    ?

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 // Consult https://vitejs.dev/config/ to learn about these options
 import { resolve } from 'path';
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import { cwd } from 'process';
 
+const pkg = JSON.parse(readFileSync(join(cwd(), 'package.json')));
+
+/** @type {import('vite').UserConfig} */
 export default {
+	ssr: {
+		noExternal: Object.keys(pkg.dependencies || {})
+	},
 	resolve: {
 		alias: {
 			$components: resolve('src/app/components'),
